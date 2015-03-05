944de593af07cb58d2ff5ebd0dcf98bc3d2aab41
Grzesiu <grzesiug44+github@gmail.com>
2015-03-05 21:47:13 +0100

    Removed comment

--- deadline24/2015/src/roads.py
+++ deadline24/2015/src/roads.py
@@ -50,52 +50,7 @@ def save_answer((T, answer)):
 			print id,
 		print
 	
-			
-		
-"""	
-def score(input, output):
-	with open(input) as fi, stdinSwapper(fi):
-		
-		
-	T0 = sum( max(abs(x)+abs(y), b)+s+abs(x)+abs(y) for id,x,y,b,e,d,s in D.values() )
-	
-	with open(output) as fo, stdinSwapper(fo):
-		K, T1 = iline()
-		visited = set()
-		T = 0
-		
-		for i in xrange(K):
-			t = vx = vy = q = 0
-			for id in iline():
-				id, x, y, b, e, d, s = D[id]
-				
-				t = max( t + abs(x-vx) + abs(y-vy), b ) + s
-				T += abs(x-vx) + abs(y-vy)
-				vx, vy = x, y
-				
-				if id in visited:
-					raise Exception('Revisiting client')
-					
-				visited.add(id)
-				
-				if t > e+s:
-					raise Exception('Truck is too late')
-					
-				q += d
-				
-			T += abs(vx) + abs(vy)
-				
-			if q > Q:
-				raise Exception('Truck is too small')
-			
-		if len(visited) != C:
-			raise Exception('Not all clients are satisfied')
-				
-		if T != T1:
-			raise Exception('Invalid score')
-		
-	return 
-					"""
+
 					
 if __name__ == '__main__':
 	datasets = load_dataset()
